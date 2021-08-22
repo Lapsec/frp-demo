7e401680fc3c285bcbd7d58c45d04348e3f02bf5
lapsec <lapse.c@foxmail.com>
2021-08-22 23:55:50 +0800

    fix: header

--- src/service/header.js
+++ src/service/header.js
@@ -1,64 +1,73 @@
 import { from, BehaviorSubject } from 'rxjs';
 // import { filter } from 'rxjs/operators'
-
-const fetchHeaderRes = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve({
-      campusId: '2',
-      gradeId: '2',
-      classId: '2',
-      panelGroup: [
+const mockHeaderData = {
+  campusId: '2',
+  gradeId: '2',
+  classId: '2',
+  panelGroup: [
+    {
+      name: 'apple',
+      id: 'group1',
+      panelList: [
         {
-          name: 'apple',
-          id: 'group1',
-          panelList: [
-            {
-              id: 'ipad_pro',
-              name: 'group1 panel1',
-            },
-            {
-              id: 'iphone12',
-              name: 'group2 panel2',
-            }
-          ],
+          id: 'ipad_pro',
+          name: 'group1 panel1',
         },
         {
-          name: 'tesla',
-          id: 'group2',
-          panelList: [
-            {
-              id: 'teslabot',
-              name: 'teslabot',
-            },
-            {
-              id: 'models',
-              name: 'models',
-            }
-          ],
-        },
-        {
-          name: 'microsoft',
-          id: 'group3',
-          panelList: [
-            {
-              id: 'windows',
-              name: 'windows',
-            },
-            {
-              id: 'onenote',
-              name: 'onenote',
-            }
-          ],
+          id: 'iphone12',
+          name: 'group2 panel2',
         }
-      ]
-    })
-  }, 500)
-})
+      ],
+    },
+    {
+      name: 'tesla',
+      id: 'group2',
+      panelList: [
+        {
+          id: 'teslabot',
+          name: 'teslabot',
+        },
+        {
+          id: 'models',
+          name: 'models',
+        }
+      ],
+    },
+    {
+      name: 'microsoft',
+      id: 'group3',
+      panelList: [
+        {
+          id: 'windows',
+          name: 'windows',
+        },
+        {
+          id: 'onenote',
+          name: 'onenote',
+        }
+      ],
+    }
+  ]
+};
+
+const fetchHeaderData = (req: any) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(
+        {
+          ...mockHeaderData,
+          ...req,
+        }
+      );
+    }, 500)
+  })
+}
+
 
 // 这个promise期望能触发一个subject
 // 同时完成cache存储
 export const headerBehaviorSubject$ = new BehaviorSubject({});
-export const headerfetchDataObservable$ = from(fetchHeaderRes);
+export const headerfetchDataObservable$ = from(fetchHeaderData({}));
 
 headerfetchDataObservable$.subscribe(headerBehaviorSubject$);
 
